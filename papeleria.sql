3cce0352b6bb2402031597ab2902ecdf77b55eca
Alexia Asunción <alexiaap_304@hotmail.com>
2024-06-29 20:41:09 -0500

    BD modificada

--- papeleria.sql
+++ papeleria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-06-2024 a las 06:53:01
+-- Tiempo de generación: 30-06-2024 a las 03:39:26
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `categoria` (
-  `idCategoria` int(3) NOT NULL,
+  `idcategoria` int(3) NOT NULL,
   `categoria` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -36,9 +36,32 @@ CREATE TABLE `categoria` (
 -- Volcado de datos para la tabla `categoria`
 --
 
-INSERT INTO `categoria` (`idCategoria`, `categoria`) VALUES
+INSERT INTO `categoria` (`idcategoria`, `categoria`) VALUES
 (1, 'Papeles'),
 (2, 'Lapices');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalleproforma`
+--
+
+CREATE TABLE `detalleproforma` (
+  `iddetalleproforma` int(3) NOT NULL,
+  `idproforma` int(3) NOT NULL,
+  `idproducto` int(3) NOT NULL,
+  `cantidad` int(4) NOT NULL,
+  `subtotal` float(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `detalleproforma`
+--
+
+INSERT INTO `detalleproforma` (`iddetalleproforma`, `idproforma`, `idproducto`, `cantidad`, `subtotal`) VALUES
+(3, 3, 1, 10, 1.00),
+(4, 4, 1, 5, 0.50),
+(5, 4, 2, 4, 4.00);
 
 -- --------------------------------------------------------
 
@@ -47,10 +70,10 @@ INSERT INTO `categoria` (`idCategoria`, `categoria`) VALUES
 --
 
 CREATE TABLE `producto` (
-  `idProducto` int(3) NOT NULL,
+  `idproducto` int(3) NOT NULL,
   `codigo` varchar(10) NOT NULL,
   `nombre` varchar(50) NOT NULL,
-  `idCategoria` int(3) NOT NULL,
+  `idcategoria` int(3) NOT NULL,
   `precio` float(5,2) NOT NULL,
   `stock` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -59,10 +82,34 @@ CREATE TABLE `producto` (
 -- Volcado de datos para la tabla `producto`
 --
 
-INSERT INTO `producto` (`idProducto`, `codigo`, `nombre`, `idCategoria`, `precio`, `stock`) VALUES
+INSERT INTO `producto` (`idproducto`, `codigo`, `nombre`, `idcategoria`, `precio`, `stock`) VALUES
 (1, 'A001', 'Hoja Bond A4', 1, 0.10, 100),
 (2, 'B001', 'Lapiz 2B Artesco', 2, 1.00, 25),
 (3, 'A002', 'Hoja Bulky A4', 1, 0.20, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proforma`
+--
+
+CREATE TABLE `proforma` (
+  `idproforma` int(3) NOT NULL,
+  `idusuario` int(2) NOT NULL,
+  `fecha` date NOT NULL,
+  `estado` varchar(20) NOT NULL,
+  `totalProforma` float(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `proforma`
+--
+
+INSERT INTO `proforma` (`idproforma`, `idusuario`, `fecha`, `estado`, `totalProforma`) VALUES
+(1, 1, '2024-06-30', 'Pendiente', 0.50),
+(2, 1, '2024-06-30', 'Pendiente', 3.00),
+(3, 1, '2024-06-30', 'Pendiente', 1.00),
+(4, 1, '2024-06-30', 'Pendiente', 4.50);
 
 --
 -- Índices para tablas volcadas
@@ -72,14 +119,28 @@ INSERT INTO `producto` (`idProducto`, `codigo`, `nombre`, `idCategoria`, `precio
 -- Indices de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  ADD PRIMARY KEY (`idCategoria`);
+  ADD PRIMARY KEY (`idcategoria`);
+
+--
+-- Indices de la tabla `detalleproforma`
+--
+ALTER TABLE `detalleproforma`
+  ADD PRIMARY KEY (`iddetalleproforma`),
+  ADD KEY `idproducto` (`idproducto`),
+  ADD KEY `idproforma` (`idproforma`);
 
 --
 -- Indices de la tabla `producto`
 --
 ALTER TABLE `producto`
-  ADD PRIMARY KEY (`idProducto`),
-  ADD KEY `idCategoria` (`idCategoria`);
+  ADD PRIMARY KEY (`idproducto`),
+  ADD KEY `idcategoria` (`idcategoria`);
+
+--
+-- Indices de la tabla `proforma`
+--
+ALTER TABLE `proforma`
+  ADD PRIMARY KEY (`idproforma`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -89,23 +150,42 @@ ALTER TABLE `producto`
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `idCategoria` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idcategoria` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `detalleproforma`
+--
+ALTER TABLE `detalleproforma`
+  MODIFY `iddetalleproforma` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `idProducto` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idproducto` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `proforma`
+--
+ALTER TABLE `proforma`
+  MODIFY `idproforma` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
+-- Filtros para la tabla `detalleproforma`
+--
+ALTER TABLE `detalleproforma`
+  ADD CONSTRAINT `detalleproforma_ibfk_1` FOREIGN KEY (`idproducto`) REFERENCES `producto` (`idproducto`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `detalleproforma_ibfk_2` FOREIGN KEY (`idproforma`) REFERENCES `proforma` (`idproforma`);
+
+--
 -- Filtros para la tabla `producto`
 --
 ALTER TABLE `producto`
-  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`idCategoria`) REFERENCES `categoria` (`idCategoria`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`idcategoria`) REFERENCES `categoria` (`idCategoria`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
